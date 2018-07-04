f56ee01705cba7e77deb0f0f3e37540828cbafea
谭上鸥 <tanshangou@hotmail.com>
2018-07-04 14:21:42 +0800

    新增struts注解插件

---
+++ src/main/java/action/LoginAction.java
@@ -0,0 +1,14 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.ParentPackage;
+
+/**
+ * @anthor tanshangou
+ * @time 2018/7/4
+ * @description
+ */
+@ParentPackage("json-default")
+public class LoginAction extends ActionSupport{
+
+}
